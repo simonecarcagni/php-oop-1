e9ec30bda31520b38a85fdac369c85dd8380f7b4
Simone Carcagnì <simonecarcagni@libero.it>
2023-05-08 17:22:28 +0200

    Esercizio Svolto

---
+++ Models/Movie.php
@@ -0,0 +1,20 @@
+<?php
+
+class Movie {
+    public $title;
+
+    public $image;
+
+    public $timing;
+
+    public function __construct($_title, $_image, $_timing){
+        $this->title = $_title;
+        $this->image = $_image;
+        $this->timing = $_timing;
+    }
+
+    public function getMovierDetai(){
+        return "Movie: $this->title, Duration: $this->timing, Poster: $this->poster";
+    }
+}
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . "/Models/Movie.php";
+
+$starWars = new Movie('Star Wars, la minacciafantasma', 'ciao', 120);
+
+$avatar2 = new Movie('Star Wars, la minacciafantasma', 'ciao', 120);
+
+$avengersInfinityWar = new Movie('Star Wars, la minacciafantasma', 'images/avengersinfinitywar.jpeg', 120);
